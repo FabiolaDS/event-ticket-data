72a6264d27d54209f47991491868e4235bb20b3e
Lia Cicati <LiaCicati@yandex.com>
2021-10-12 16:26:58 +0200

    1st commit

---
+++ src/main/java/com/eventtickets/datatier/model/Event.java
@@ -0,0 +1,21 @@
+package com.eventtickets.datatier.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event
+{
+  private Long id;
+  private String name;
+  private String description;
+  private String location;
+  private String thumbnail;
+  private int nrOfTickets;
+  private boolean isCancelled;
+  private LocalDateTime dateTime;
+}
