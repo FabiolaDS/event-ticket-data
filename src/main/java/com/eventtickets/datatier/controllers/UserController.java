6cef7691fe83343c9f83850508c12ad6f488ebb3
Fabiola <285055@via.dk>
2021-10-26 17:19:33 +0200

    implement PATCH method handing for users

--- src/main/java/com/eventtickets/datatier/controllers/UserController.java
+++ src/main/java/com/eventtickets/datatier/controllers/UserController.java
@@ -4,9 +4,8 @@ import com.eventtickets.datatier.model.User;
 import com.eventtickets.datatier.persistence.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/users")
 @RestController
-
+@RequestMapping("/users")
 public class UserController {
     private UserRepository userRepository;
 
@@ -22,5 +21,26 @@ public class UserController {
     @GetMapping
     public User findUserByEmail(@RequestParam String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @PatchMapping("/{userId}")
+    public User updateUser(@PathVariable long userId, @RequestBody User updateData) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("No such user"));
+
+        if(updateData.getEmail() != null) {
+            user.setEmail(updateData.getEmail());
+        }
+        if(updateData.getFullName() != null) {
+            user.setFullName(updateData.getFullName());
+        }
+        if(updateData.getPassword() != null) {
+            user.setPassword(updateData.getPassword());
+        }
+        if(updateData.getIsAdmin() != null) {
+            user.setIsAdmin(updateData.getIsAdmin());
+        }
+
+        return userRepository.save(user);
     }
 }
